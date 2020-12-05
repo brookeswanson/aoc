df19eecfc64a0425b7a1e2fd51a68239732fb80d
bswanson <brookeswanson09@gmail.com>
2020-12-05 17:03:22 -0600

    Day 4 clean up.

--- src/aoc/solutions/year2020.clj
+++ src/aoc/solutions/year2020.clj
@@ -1,5 +1,6 @@
 (ns aoc.year2020
   (:require [aoc.util :as util]
+            [clojure.set :as clj-set]
             [clojure.string :as string]))
 
 ;; day 1
@@ -94,13 +95,52 @@
   []
   (let [data (util/read-file "2020/day3.txt" (partial into []))]
     {:part1 (day3-parse {:right 3})
-     :part2 (apply +
-                   (partial day3-parse data)
-                   [{:right 3}
-                    {:right 5}
-                    {:down 2 :right 1}
-                    {:right 1}
-                    {:right 7}])}))
+     :part2 (apply + (map (partial day3-parse data)
+                          [{:right 1}
+                           {:right 3}
+                           {:right 5}
+                           {:right 7}
+                           {:down 2 :right 1}]))}))
+
+;; day 4
+(def necessary-keys
+  #{:pid :ecl :hcl :hgt :eyr :iyr :byr})
+
+(defn ->map [pass]
+  (let [parts (clojure.string/split pass #"\s+")
+        keys (into #{} (map (comp
+                             keyword
+                             first
+                             #(clojure.string/split % #":")) parts))]
+    (= (count necessary-keys)
+       (count (clj-set/intersection necessary-keys keys)))))
+
+(defn valid-height?
+  [height]
+  (let [[_ num unit] (re-find #"(\d+)(in|cm)" height)]
+    (case unit
+      "in" (<= 59 (read-string num) 76)
+      "cm" (<= 150 (read-string num) 193)
+      false)))
+
+(defn ->validate [pass]
+  (let [parts (clojure.string/split pass #"\s+")
+        passport-fields (map #(clojure.string/split % #":") parts)
+        {:strs [byr iyr eyr ecl pid hcl hgt cid]} passport-fields]
+    (and (and byr iyr eyr ecl pid hcl hgt)
+         (<= 1920 (read-string byr) 2002)
+         (<= 2010 (read-string iyr) 2020)
+         (<= 2020 (read-string eyr) 2030)
+         (some #{ecl} ["amb" "blu" "brn" "gry" "grn" "hzl" "oth"])
+         (re-find #"#[0-9a-f]{6}" hcl)
+         (re-find #"^\d{9}$" pid)
+         (valid-height? hgt))))
+
+(defn day4
+  []
+  (let [data (string/split-lines (util/simple-read-file "2020/day4.txt" false) "\n\n")]
+    {:part1 (->> (map ->map data) (remove identity) count)
+     :part2 (->> (map ->validate data) (remove identity) count)}))
 
 ;; day 5
 (defn do-work
